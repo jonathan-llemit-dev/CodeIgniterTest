9875d3285e24f0e93dbfa39fc5818943c65ed186
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-22 21:12:15 +0800

    index function on auth controller added

--- application/controllers/auth/AuthController.php
+++ application/controllers/auth/AuthController.php
@@ -3,5 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class AuthController extends CI_Controller {
 
+	public function index()
+	{
+
+		$this->load->view('auth/login');
+
+	}
 
 }
