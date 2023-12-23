edd90a75a50ac8e8fd162273aa22d86702a1010b
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-23 19:46:55 +0800

    register page added margin on bottom

--- application/views/auth/register.php
+++ application/views/auth/register.php
@@ -15,7 +15,7 @@
   </head>
   <body>
 
-    <div class="container mt-5">
+    <div class="container mt-5 mb-5">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
