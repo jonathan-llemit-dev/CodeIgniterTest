381218146bfb759f7f097f8800636940e2d414f1
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-24 17:18:59 +0800

    store function added on auth controller

--- application/controllers/auth/AuthController.php
+++ application/controllers/auth/AuthController.php
@@ -17,4 +17,35 @@ class AuthController extends CI_Controller {
 
 	}
 
+	public function store()
+	{
+
+        $this->load->library('form_validation'); 
+
+        $this->form_validation->set_rules('name', 'Name', 'required|alpha');
+        $this->form_validation->set_rules('age', 'Age', 'required|numeric');
+        $this->form_validation->set_rules('gender', 'Gender', 'required');
+
+        if ($this->form_validation->run() == false) {
+
+            return $this->create();
+
+        }
+
+        $StudentModel = new StudentModel;
+
+        $data = [
+            'student_name' => $this->input->post('name'),
+            'age' => $this->input->post('age'),
+            'gender' => $this->input->post('gender'),
+        ];
+
+        $StudentModel->insert_data($data);
+
+        $this->session->set_flashdata('created', 'New Student Added!');
+
+		return redirect('students_portal');
+
+	}
+
 }
