4aa3881ad7f3dbf27eb667e098f64a030ea923cf
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-24 17:41:07 +0800

    validation for email and password added

--- application/controllers/auth/AuthController.php
+++ application/controllers/auth/AuthController.php
@@ -25,8 +25,10 @@ class AuthController extends CI_Controller {
         $this->form_validation->set_rules('firstName', 'First Name', 'required|alpha');
 		$this->form_validation->set_rules('lastName', 'Last Name', 'required|alpha');
 		$this->form_validation->set_rules('birthDate', 'Birth Date', 'required|callback_check_age');
-        $this->form_validation->set_rules('age', 'Age', 'required|numeric');
         $this->form_validation->set_rules('gender', 'Gender', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+		$this->form_validation->set_rules('password', 'Password', 'required|min_length[8]|callback_validate_password');
+		$this->form_validation->set_rules('confirmPassword', 'Confirm Password', 'required|matches[password]');
 
         if ($this->form_validation->run() == false) {
 
@@ -64,6 +66,14 @@ class AuthController extends CI_Controller {
 			return false;
 		}
 	
+		return true;
+	}
+
+	public function validate_password($password) {
+		if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/', $password)) {
+			$this->form_validation->set_message('validate_password', 'The password must contain at least one lowercase letter, one uppercase letter, one number, one symbol, and be at least 8 characters long.');
+			return false;
+		}
 		return true;
 	}
 
