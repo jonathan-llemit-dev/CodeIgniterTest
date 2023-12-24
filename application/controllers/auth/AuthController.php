aef4cd205624244c40c9b03daefbe900da900702
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-24 17:33:03 +0800

    added validation for names and birthdate

--- application/controllers/auth/AuthController.php
+++ application/controllers/auth/AuthController.php
@@ -22,7 +22,9 @@ class AuthController extends CI_Controller {
 
         $this->load->library('form_validation'); 
 
-        $this->form_validation->set_rules('name', 'Name', 'required|alpha');
+        $this->form_validation->set_rules('firstName', 'First Name', 'required|alpha');
+		$this->form_validation->set_rules('lastName', 'Last Name', 'required|alpha');
+		$this->form_validation->set_rules('birthDate', 'Birth Date', 'required|callback_check_age');
         $this->form_validation->set_rules('age', 'Age', 'required|numeric');
         $this->form_validation->set_rules('gender', 'Gender', 'required');
 
@@ -46,6 +48,23 @@ class AuthController extends CI_Controller {
 
 		return redirect('students_portal');
 
+	}
+
+	public function check_age($birthdate) {
+		// Convert birthdate to DateTime object
+		$birthdate = new DateTime($birthdate);
+	
+		// Calculate age
+		$today = new DateTime();
+		$age = $today->diff($birthdate)->y;
+	
+		// Check if age is at least 18
+		if ($age < 18) {
+			$this->form_validation->set_message('check_age', 'You must be at least 18 years old.');
+			return false;
+		}
+	
+		return true;
 	}
 
 }
