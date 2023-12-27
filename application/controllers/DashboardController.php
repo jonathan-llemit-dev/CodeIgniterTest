29a25af7fb1ef6fa1947fca4374c4496b2a1480f
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-27 23:21:43 +0800

    DashboardController added

---
+++ application/controllers/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class DashboardController extends CI_Controller
+{
+
+    public function index()
+	{
+
+        $this->load->view('dashboard');
+
+	}
+
+}
