fed1ec5e2265ff4a38a9cc72eb851b39a1f3d344
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2023-12-28 12:38:51 +0800

    authenticate method added on model

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -15,6 +15,24 @@ class UserModel extends CI_Model {
 
   }
 
+  public function authenticate($user_email, $password) {
+    // Fetch user from the database based on the provided username
+    $query = $this->db->get_where('users', array('user_email' => $user_email));
+
+    if ($query->num_rows() > 0) {
+        $user = $query->row();
+
+        // Verify the password (you should use a more secure method, like password_hash())
+        if ($user->password == $password) {
+            // Password is correct, return true
+            return true;
+        }
+    }
+
+    // Authentication failed
+    return false;
+  }
+
 }
 
 /* End of file UserModel_model.php */
