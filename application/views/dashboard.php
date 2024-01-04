f52e0b9f690180cf6a1533e76b8e24a40d4aa64d
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-01-04 21:18:21 +0800

    logout applied

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -43,7 +43,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link" href="<?= base_url('logout'); ?>">
                                 Logout
                             </a>
                         </li>
