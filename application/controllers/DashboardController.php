f609b06f85638eea1a3be69ac5ad001522b845b7
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-02-04 19:19:42 +0800

    api request using curl tried

--- application/controllers/DashboardController.php
+++ application/controllers/DashboardController.php
@@ -16,16 +16,36 @@ class DashboardController extends CI_Controller
     public function index()
 	{
 
-        if ($this->session->userdata('logged_in')) {
-            $data['first_name'] = $this->session->userdata('first_name');
-            $data['last_name'] = $this->session->userdata('last_name');
-            $data['birthdate'] = $this->session->userdata('birthdate');
-            $data['gender'] = $this->session->userdata('gender');
-            $data['email'] = $this->session->userdata('userEmail');
-            $this->load->view('dashboard', $data);
-        } else {
-            redirect('login');
-        }
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://www.deckofcardsapi.com/api/deck/new/draw/?count=1',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        $response = json_decode($response, true);
+        var_dump($response);
+
+        // if ($this->session->userdata('logged_in')) {
+        //     $data['first_name'] = $this->session->userdata('first_name');
+        //     $data['last_name'] = $this->session->userdata('last_name');
+        //     $data['birthdate'] = $this->session->userdata('birthdate');
+        //     $data['gender'] = $this->session->userdata('gender');
+        //     $data['email'] = $this->session->userdata('userEmail');
+        //     $this->load->view('dashboard', $data);
+        // } else {
+        //     redirect('login');
+        // }
 
 	}
 
