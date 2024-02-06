cd689bbe9d8a0a2759d1985a1b85e2fd4624f555
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-02-06 21:01:15 +0800

    sample curl

--- application/controllers/DashboardController.php
+++ application/controllers/DashboardController.php
@@ -16,24 +16,66 @@ class DashboardController extends CI_Controller
     public function index()
 	{
 
+        // $curl = curl_init();
+
+        // curl_setopt_array($curl, array(
+        // CURLOPT_URL => 'https://www.deckofcardsapi.com/api/deck/new/draw/?count=1',
+        // CURLOPT_RETURNTRANSFER => true,
+        // CURLOPT_ENCODING => '',
+        // CURLOPT_MAXREDIRS => 10,
+        // CURLOPT_TIMEOUT => 0,
+        // CURLOPT_FOLLOWLOCATION => true,
+        // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        // CURLOPT_CUSTOMREQUEST => 'GET',
+        // ));
+
+        // $response = curl_exec($curl);
+
+        // curl_close($curl);
+
+        // $response = json_decode($response, true);
+
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://www.deckofcardsapi.com/api/deck/new/draw/?count=1',
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
+          CURLOPT_URL => 'https://192.168.162.12/payment/pay_without_ref',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS =>'{
+            "info": {
+                "user": "jeraldtc@yahoo.com",
+                "posted_by": "jerald",
+                "branch": "20325"
+            },
+            "payment": {
+                "first_name": "Jerald",
+                "middle_name": "Jerald",
+                "last_name": "Jerald",
+                "email": "jerald@clearmindai.com",
+                "mobile_no": "09999999999",
+                "amount": "100",
+                "total_amount": "100",
+                "remarks": "UAT Payment"
+            },
+            "order_no": "KJQMJM84",
+            "OR_no": "USSC123"
+        }',
+          CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json',
+            'Authorization: Basic Og=='
+          ),
         ));
-
+        
         $response = curl_exec($curl);
-
+        
         curl_close($curl);
-
         $response = json_decode($response, true);
+
         var_dump($response);
 
         // if ($this->session->userdata('logged_in')) {
